5939e49220a5af1378e618ad30622075a71938e9
Stanislav <37810969+strelnikovstanislav@users.noreply.github.com>
2018-05-02 21:02:17 +0300

    added myfirst.cpp

---
+++ myfirst.cpp
@@ -0,0 +1,12 @@
+// myfirst.cpp
+
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+    cout << "You won't regret it!" << endl;
+    // cin.get();
+    return 0;
+}
